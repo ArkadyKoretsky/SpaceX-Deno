3056cfe7f6f6ddb76810a20703767c340d562da4
Arkady Koretsky <arkady1992@gmail.com>
2020-08-31 20:55:49 +0300

    Added mod.ts file

---
+++ mod.ts
@@ -0,0 +1,15 @@
+import { Application } from 'https://deno.land/x/oak@v6.1.0/mod.ts';
+
+const app = new Application();
+const PORT = 8000;
+
+app.use((ctx) => {
+  ctx.response.body = 'Mission Control API 🚀';
+});
+
+if (import.meta.main) {
+  console.log(`server is running on port ${PORT}`);
+  await app.listen({
+    port: PORT,
+  });
+}
