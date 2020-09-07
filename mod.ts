90241a3879714f27bd6c705c1960360f92229c8b
Arkady Koretsky <arkady1992@gmail.com>
2020-09-07 19:28:29 +0300

    Added Response Times

--- mod.ts
+++ mod.ts
@@ -5,11 +5,20 @@ const PORT = 8000;
 
 app.use(async (ctx, next) => {
   await next();
-  console.log(`${ctx.request.method} ${ctx.request.url}`);
+  const time = ctx.response.headers.get('X-Response-Time');
+  console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`);
 });
 
-app.use((ctx) => {
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const delta = Date.now() - start;
+  ctx.response.headers.set('X-Response-Time', `${delta}ms`);
+});
+
+app.use(async (ctx, next) => {
   ctx.response.body = 'Mission Control API 🚀';
+  await next();
 });
 
 if (import.meta.main) {
